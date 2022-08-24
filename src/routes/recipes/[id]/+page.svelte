5167c7d90f71e124aad7778241bbffde0d635a7a
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2022-08-24 14:14:45 +0100

    created single recipe page and module

---
+++ src/routes/recipes/[id]/+page.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+import { onMount } from "svelte";
+
+
+    /** @type {import('./$types').PageData} */
+    export let data;
+    
+    let { recipe } = data.props;
+
+    // console.log(recipe);
+
+    onMount(()=> {
+        if(recipe.instructions) document.querySelector('#recipeInfo').innerHTML = recipe.instructions;
+    })
+    
+</script>
+<main>
+    <h1>{recipe.title}</h1>
+
+    <img src={recipe.image} alt={recipe.title}>
+
+    <div>
+        <h2>Ingredients</h2>
+        <ul>
+            {#each recipe.extendedIngredients as ingredient}
+                <li>{ingredient.name}</li>
+            {/each}
+        </ul>
+        
+        <h2>Cuisines</h2>
+        <ul>
+            {#each recipe.cuisines as cusine}
+                <li>{cusine}</li>
+            {/each}
+        </ul>
+
+        <h2>Diets</h2>
+        <ul>
+            {#each recipe.diets as diet}
+                <li>{diet}</li>
+            {/each}
+        </ul>
+
+        {#if recipe.instructions}
+        <h2>Instructions</h2>
+        <div id="recipeInfo"></div>
+        {/if}
+    </div>
+</main>
---
+++ src/routes/recipes/[id]/+page.ts
@@ -0,0 +1,28 @@
+
+
+/** @type {import('./$types').PageLoad} */
+export async function load(page: any){
+    let data:any = [];
+    // console.log(page);
+    
+    let { fetch } = page;
+    try {
+        const id = page.params.id;
+        const apiKey = import.meta.env.VITE_SPOONACULAR_API_KEY;
+                
+        let res = await fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${apiKey}`);
+        
+        const resData = await res.json();
+        data = resData
+        
+    } catch (error) {
+        // console.error(error);
+    }finally{
+        return {
+            props: {
+                recipe : data
+            }
+        }
+    }
+
+}
