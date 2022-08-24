107a697975172f709c1d5c5e46ea555bcb71ace1
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2022-08-24 14:14:31 +0100

    created recipes page and module

---
+++ src/routes/recipes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    /** @type {import('./$types').PageData} */
+    export let data;
+    
+    let { recipes } = data.props;
+</script>
+
+<main>
+    <title>Recipes</title>
+    <h1>Food Recipes</h1>
+
+    {#each recipes as item}
+        <a href={`/recipes/${item.id}`}>
+            <p>{item.title}</p>
+            <img src={item.image} alt={item.title} />
+        </a>
+    {/each}
+</main>
---
+++ src/routes/recipes/+page.ts
@@ -0,0 +1,21 @@
+/** @type {import('./$types').PageLoad} */
+export async function load(page: any){
+    let data:any = [];
+    let { fetch } = page;
+    try {
+        const apiKey = import.meta.env.VITE_SPOONACULAR_API_KEY;
+        let res = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}`);
+        
+        const { results } = await res.json();
+        data = results;
+    } catch (error) {
+        console.error(error);
+    }finally{
+        return {
+            props: {
+                recipes : data,
+            }
+        }
+    }
+
+}
