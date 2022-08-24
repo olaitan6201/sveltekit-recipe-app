268bd9dce5d223c186a157621a1bf8c63e468b6f
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2022-08-24 14:56:28 +0100

    tailwindcss configured

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
-import Footer from "../components/Footer.svelte";
-import Nav from "../components/Nav.svelte";
-
+    import Footer from "../components/Footer.svelte";
+    import Nav from "../components/Nav.svelte";
+    import "../app.css";
 </script>
 
 <main>
