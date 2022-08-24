bbbb1a73cd0cb62fdd1d1de14e27e0b658d66f33
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2022-08-24 14:13:47 +0100

    created layout file

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+import Footer from "../components/Footer.svelte";
+import Nav from "../components/Nav.svelte";
+
+</script>
+
+<main>
+    <Nav />
+    
+    <slot></slot>
+
+    <Footer />
+</main>
